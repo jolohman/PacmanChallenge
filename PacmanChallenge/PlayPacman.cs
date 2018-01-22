73a559753e95a597fb4f29b79f609ee1d43691ed
Josh Lohman <jjosh9708@gmail.com>
2018-01-22 11:24:31 -0500

    pushing PlayPacman, but needs a lot of work

--- PacmanChallenge/PlayPacman.cs
+++ PacmanChallenge/PlayPacman.cs
@@ -7,34 +7,34 @@ using System.Threading.Tasks;
 
 namespace PacmanChallenge
 {
-    class PlayPacman : PlayerInput
+    public static class PlayPacman
     {
         public static void Begin()
         {
-            GetGamePlayInfo;
-        }
-
-        //Constructors
-        public PlayPacman()
-        {
-            this.IsAlive = true;
-            this.TotalLives = 3;
-            this.TotalPoints = 5000;
-            this.TotalLivesGained = 0;
-        }
+            var runGame = GetGamePlayInfo;
 
 
-        //Methods
-        public int EatingMachine()
-        {
+            //Constructors
+            PlayPacman()
+            {
+                this.IsAlive = true;
+                this.TotalLives = 3;
+                this.TotalPoints = 5000;
+                this.TotalLivesGained = 0;
+            }
 
-            return 0;
+
+            //Methods
+            int EatingMachine()
+            {
+
+                return 0;
+            }
         }
 
         public static string[] GetGamePlayInfo()
         {
-            var path = @"C:\DotNetProjects\PacmanChallenge\KataPacman-seq.txt";
-            var file = File.ReadAllText(path);
+            var file = File.ReadAllText(@"C:\DotNetProjects\PacmanChallenge\KataPacman-seq.txt");
             string[] stream = file.Split(',');
             return stream;
         }
